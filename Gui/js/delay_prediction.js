f41fee7a21488ac3714110b7b93f9817dec96c2a
Akanksha Bhardwaj <bhardwaj.akanksha21@gmail.com>
2018-03-17 19:38:16 +0530

    update UI, add confidence field

--- Gui/js/delay_prediction.js
+++ Gui/js/delay_prediction.js
@@ -1,72 +1,49 @@
 var $form           = $('.js-train-prediction-form');
-var $trainNumberInput     = $('.js-train-number-input');
+var $trainNumberInput = $('.js-train-number-input');
+var $errorsOutput  = $('.js-errors');
 var $dateInput   = $('.js-date-input');
 var $hourOutput  = $('.js-delay');
-var $errorsOutput   = $('.js-errors');
-
+var $confidence   = $('.js-confidence')
 
 function compute() {
-  var trainNumber     = $trainNumberInput.val();
+  var trainNumber    = $trainNumberInput.val();
   var date           = $dateInput.val();
 
   if( trainNumber == "" ) {
     $errorsOutput.html("Please enter a train number");
   }
-
-  //var minutesInDecimal  = (minutes / 60).toFixed(2)
   
-  // var convertedHours    = parseFloat(hours);
-  // var convertedMinutes  = parseFloat(minutesInDecimal);
-  // var total             = convertedHours += convertedMinutes;
- var date_string = date.toString();
+ var date1 = date.toString();
  var train_string = trainNumber.toString()
- var input = train_string + " " + date_string
+ //var date_string = new Date(date1).toDateString("yyyy-MM-dd");
+ var input = train_string + " " + date1
 
 // call python script
-
+//console.log(date_string);
   //response= runPyScript(input);
   response = "more than 4 hours"
-  console.log(response);
+  console.log(input);
   // re
   $hourOutput.html(response);
+  $confidence.html(response);
 };
 
 function runPyScript(input){
-    var jqXHR = $.ajax({
+    var obj = $.ajax({
         type: "POST",
         url: "/model_v1.py",
         async: false,
         data: { param: input }
     });
-    // format response text
-    return jqXHR.responseText;
-}
-function limitMinutesLength() {
-  var maxChars = 2;
-
-  if ($minutesInput.val().length > maxChars) {
-      $minutesInput.val($minutesInput.val().substr(0, maxChars));
-  };
-};
-
-function limitMinutesRange() {
-  var maxMinutes = 59;
-
-  if( $minutesInput.val() > 59 || $minutesInput.val() < 0) {
-    $errorsOutput.html("Minutes must be between 0 and 59");
-  } else {
-    $errorsOutput.html("");
-  };
+    return obj.responseText;
 }
 
 $(document).ready(function() {
-  $trainNumberInput.on('blur', compute);
-  
+  $dateInput.on('blur', compute); 
 
-  $trainNumberInput.on('keyup', function() {
-    //limitMinutesLength();
+  // $trainNumberInput.on('keyup', function() {
     
-  });
+  // });
 
 });
   
