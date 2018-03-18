37580d7a391e112842ca23b32952ad581820125a
Akanksha Bhardwaj <bhardwaj.akanksha21@gmail.com>
2018-03-18 09:16:35 +0530

    update Gui with button

--- Gui/delay_prediction.js
+++ Gui/delay_prediction.js
@@ -6,30 +6,40 @@ function compute() {
   var $errorsOutput  = $('.js-errors');
   var $dateInput   = $('.js-date-input');
   var $hourOutput  = $('.js-delay');
-  var $confidence   = $('.js-confidence')
+  //var $confidence   = $('.js-confidence')
 
   var trainNumber    = $trainNumberInput.val();
   var date           = $dateInput.val();
 
   // validate 
-
-  if( trainNumber == "" ) {
-    $errorsOutput.html("Please enter a train number");
+   var date1 = date.toString();
+   var train_string = trainNumber.toString()
+  if( train_string == "0" && date1 == "") {
+    $errorsOutput.html("Please enter a train number and a date.");
   }
-  
- var date1 = date.toString();
- var train_string = trainNumber.toString()
- //var date_string = new Date(date1).toDateString("yyyy-MM-dd");
- var input = train_string + " " + date1
+   else if( train_string == "0" ) {
+    $errorsOutput.html("Please enter a train number.");
+  }
+  else if( date1 == "" ) {
+    $errorsOutput.html("Please enter a date.");
+  }
+  else {
 
-// call python script
-//console.log(date_string);
-  //response= runPyScript(input);
-  response = "more than 4 hours"
-  console.log(input);
-  // re
-  $hourOutput.html(response);
-  $confidence.html(response);
+   //var date_string = new Date(date1).toDateString("yyyy-MM-dd");
+   var input = train_string + " " + date1
+
+  // call python script
+  //console.log(date_string);
+    //response= runPyScript(input);
+    response = "more than 4 hours"
+    //console.log(input);
+    // re
+    $hourOutput.html(response);
+  }
+
+  
+ 
+ // $confidence.html(response);
 };
 
 function runPyScript(input){
@@ -40,7 +50,7 @@ function runPyScript(input){
         data: { param: input }
     });
     return obj.responseText;
-}
+};
 
 // $(document).ready(function() {
 //   $trainNumberInput.on('keyup', validate1() {
